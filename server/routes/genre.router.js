2e6d119b44e8ca9c705636c885f181cdf36e7c70
Sumaia Dahirow <96860421+SumayaDahir@users.noreply.github.com>
2023-03-10 15:01:37 -0600

    Initial commit

---
+++ server/routes/genre.router.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+const pool = require("../modules/pool");
+
+router.get("/", (req, res) => {
+  const query = `SELECT movie_id, title, name FROM movies_genres
+  JOIN "movies" ON movies.id = movies_genres.movie_id
+  JOIN "genres" ON genres.id = movies_genres.genre_id;
+  `;
+  pool
+    .query(query)
+    .then((result) => {
+      res.send(result.rows);
+    })
+    .catch((err) => {
+      console.log("ERROR: Get all genres", err);
+      res.sendStatus(500);
+    });
+});
+
+module.exports = router;
